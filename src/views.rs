de36092756a1f447a565c02234fde3716c821df2
Agnish Ghosh <iamagnix@gmail.com>
2024-08-01 17:27:40 +0530

    add: prover's view of GKR

--- src/views.rs
+++ src/views.rs
@@ -19,6 +19,7 @@ impl<F: PrimeField> From <(SparseMultilinearExtension<F>, F, F)> for GKRVerifPro
 
 // Computing the product of Spare Multilinear Extension
 // with 2 other Multilinear Extensions. When dealing with GKR
+#[derive(Clone, Debug)]
 pub struct MLEProduct<F: PrimeField, MLE: MultilinearExtension<F>> (
     pub SparseMultilinearExtension<F>,
     pub MLE,
@@ -37,4 +38,39 @@ impl <F: PrimeField, MLE: MultilinearExtension<F>> From<(SparseMultilinearExtens
             Self(tuple.0, tuple.1, tuple.2)
         }
     }
+
+
+/// Sum of products as seen by the verifier
+/// For eg, for GKR, we write the verifier's view
+/// in a multivariate polynomial's General-Purpose
+/// Form
+#[derive(Clone, Debug)]
+pub struct GKRVerifSumOfProduct<F: PrimeField> {
+    pub elements: Vec<GKRVerifProduct<F>>
+}
+
+impl <F: PrimeField> From<Vec<GKRVerifProduct<F>>> for GKRVerifSumOfProduct<F> {
+    fn from (elements: Vec<GKRVerifProduct<F>>) -> Self {
+        Self {
+            elements
+        }
+    }
+}
+
+/// Sum of products as seen by the prover's view
+#[derive(Clone, Debug)]
+pub struct GKRProverSumOfProduct<F: PrimeField, MLE: MultilinearExtension<F>> {
+    pub elements: Vec<MLEProduct<F, MLE>>,
+}
+
+impl <F: PrimeField, MLE: MultilinearExtension<F>> From <Vec<MLEProduct<F, MLE>>>
+    for GKRProverSumOfProduct<F, MLE>
+
+{
+    fn from (elements: Vec<MLEProduct<F, MLE>>) -> Self {
+        Self {
+            elements
+        }
+    }
+}
 
